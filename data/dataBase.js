603444a762f8f7954c9a447c8d059002afbc342d
Jhon9191 <joaoantonio15161516@gmail.com>
2021-02-23 23:22:14 -0300

    Code refactoring pt final

---
+++ data/dataBase.js
@@ -0,0 +1,30 @@
+const perfis = [
+    { id: 1, nome: 'joao' },
+    { id: 2, nome: 'leo' }
+]
+
+const usuarios = [{
+        id: 1,
+        nome: "João",
+        email: "joao@gmail.com",
+        idade: 21,
+        perfil_id: 1
+    },
+    {
+        id: 2,
+        nome: "João",
+        email: "juyo@gmail.com",
+        idade: 61,
+        perfil_id: 1
+    },
+    {
+        id: 3,
+        nome: "João Atônio",
+        email: "jo@gmail.com",
+        idade: 31,
+        perfil_id: 2
+    },
+
+]
+
+module.exports = { usuarios, perfis }
