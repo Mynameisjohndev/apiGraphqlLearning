7e697c53e2f295952ace3b635fd6fceff26b8a37
Jhon9191 <joaoantonio15161516@gmail.com>
2021-02-23 23:28:11 -0300

    Applying enum

--- data/dataBase.js
+++ data/dataBase.js
@@ -8,21 +8,24 @@ const usuarios = [{
         nome: "João",
         email: "joao@gmail.com",
         idade: 21,
-        perfil_id: 1
+        perfil_id: 1,
+        status: "ATIVO"
     },
     {
         id: 2,
         nome: "João",
         email: "juyo@gmail.com",
         idade: 61,
-        perfil_id: 1
+        perfil_id: 1,
+        status: "INATIVO"
     },
     {
         id: 3,
         nome: "João Atônio",
         email: "jo@gmail.com",
         idade: 31,
-        perfil_id: 2
+        perfil_id: 2,
+        status: "DESATIVADO"
     },
 
 ]
