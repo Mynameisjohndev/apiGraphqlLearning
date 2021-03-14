3aff1f7577bece2b2d8bfdb41d448d58603cdc67
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-14 14:04:27 -0300

    Applying user and profile type validation

--- projetoFinal/config/context.js
+++ projetoFinal/config/context.js
@@ -1,7 +1,27 @@
+const jwt = require('jwt-simple')
+
 module.exports = async ({req}) => {
 
   await require('./simularUsuarioLogado')(req)
 
   const auth = req.headers.authorization
-  console.log(auth)
+
+  const token = auth && auth.substring(7)
+  let usuario = null
+  let admin = null
+
+  if(token) {
+    try {
+        let conteudoToken = jwt.decode(token,
+            process.env.APP_AUTH_SECRET)
+        if(new Date(conteudoToken.exp * 1000) > new Date()) {
+            usuario = conteudoToken
+        }
+    } catch(e) {
+        // token inválido
+    }
+}
+
+
+  //console.log(auth)
 }
